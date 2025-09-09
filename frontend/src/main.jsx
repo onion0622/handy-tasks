8230eaf1c7130fe2abe6a4356a63aba2e0321756
Diego Fernando Pertuz <diegofoxfire@gmail.com>
2025-09-09 01:38:05 -0500

    Actualizo frontend: cambios en App.jsx, main.jsx y nueva carpeta components

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { LavaAnimation } from './components/LavaAnimation.jsx'
+//import './components/Formulario.jsx';
+import'./components/LavaAnimation.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <LavaAnimation />
   </StrictMode>,
 )
