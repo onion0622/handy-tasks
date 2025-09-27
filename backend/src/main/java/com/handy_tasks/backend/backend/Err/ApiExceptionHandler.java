c0aa39face71d6aa05bec9edc0654d894c677f25
Dave <aguirredavid28.000@gmail.com>
2025-09-26 23:27:34 -0500

    Feat: Personalización de la response 401 UNAUTHORIZED

--- backend/src/main/java/com/handy_tasks/backend/backend/Err/ApiExceptionHandler.java
+++ backend/src/main/java/com/handy_tasks/backend/backend/Err/ApiExceptionHandler.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,5 +17,12 @@ public class ApiExceptionHandler {
         return ResponseEntity.status(HttpStatus.CONFLICT)
                              .body(Map.of("error", ex.getMessage()));
     }  
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Map<String,String>> handleBadCre(BadCredentialsException ex){
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                             .body(Map.of("error", "Usuario o contraseña invalidos..."));
+    }
     
 }
